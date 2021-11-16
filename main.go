28bb784572f94ef844d582eff43f05d7a5b8769d
yangkb09 <yangkb09@gmail.com>
2021-11-16 15:59:52 -0500

    Section 3: Saving Data to the Hard Drive

--- main.go
+++ main.go
@@ -1,13 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	// cards := newDeck()
-
-	// hand, remainingCards := deal(cards, 5)
-	// hand.print()
-	// remainingCards.print()
 	cards := newDeck()
-	fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
 }
