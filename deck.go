c010baedc701124cc1af0564301ad7e9325361a6
yangkb09 <yangkb09@gmail.com>
2021-11-17 11:21:52 -0500

    Sec 3: Shuffling a Deck

--- deck.go
+++ deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
 )
@@ -50,4 +51,12 @@ func newDeckFromFile(filename string) deck { // no receiver bc dont have deck ye
 	}
 	s := strings.Split(string(bs), ",")
 	return deck(s) // able to do this bc deck is basically []string
+}
+
+func (d deck) shuffle() {
+	for i := range d { // don't always need to reference item we're iterating over
+		newPosition := rand.Intn(len(d) - 1) // len is how we get length of slice
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
