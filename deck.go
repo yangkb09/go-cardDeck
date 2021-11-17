381cd0e95ec614f25739594b8b97df94ffc0fccd
yangkb09 <yangkb09@gmail.com>
2021-11-17 11:35:03 -0500

    Sec 3: Random Number Generation

--- deck.go
+++ deck.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"math/rand"
-	"os"
-	"strings"
+	"time"
 )
 
 // Create a new type of 'deck' which is a slice of strings
@@ -31,31 +29,33 @@ func (d deck) print() {
 	}
 }
 
-func deal(d deck, handSize int) (deck, deck) {
-	return d[:handSize], d[handSize:]
-}
+// func deal(d deck, handSize int) (deck, deck) {
+// 	return d[:handSize], d[handSize:]
+// }
 
-func (d deck) toString() string {
-	return strings.Join([]string(d), ",")
-}
+// func (d deck) toString() string {
+// 	return strings.Join([]string(d), ",")
+// }
 
-func (d deck) saveToFile(filename string) error {
-	return ioutil.WriteFile(filename, []byte(d.toString()), 0666) // return entire ln bc WriteFile returns an err
-}
+// func (d deck) saveToFile(filename string) error {
+// 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666) // return entire ln bc WriteFile returns an err
+// }
 
-func newDeckFromFile(filename string) deck { // no receiver bc dont have deck yet
-	bs, err := ioutil.ReadFile(filename)
-	if err != nil { // error handling; log err and quit program
-		fmt.Println("Error:", err)
-		os.Exit(1)
-	}
-	s := strings.Split(string(bs), ",")
-	return deck(s) // able to do this bc deck is basically []string
-}
+// func newDeckFromFile(filename string) deck { // no receiver bc dont have deck yet
+// 	bs, err := ioutil.ReadFile(filename)
+// 	if err != nil { // error handling; log err and quit program
+// 		fmt.Println("Error:", err)
+// 		os.Exit(1)
+// 	}
+// 	s := strings.Split(string(bs), ",")
+// 	return deck(s) // able to do this bc deck is basically []string
+// }
 
 func (d deck) shuffle() {
-	for i := range d { // don't always need to reference item we're iterating over
-		newPosition := rand.Intn(len(d) - 1) // len is how we get length of slice
+	source := rand.NewSource(time.Now().UnixNano()) // use cur time as seed value
+	r := rand.New(source)
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
 
 		d[i], d[newPosition] = d[newPosition], d[i]
 	}
